08e1fa101a0fb852e1699d858e5600c3dad75887
Johannes Link <business@johanneslink.net>
2011-11-26 22:58:33 +0100

    clojrue shelf move-out works

--- src-clj/storehouse.clj
+++ src-clj/storehouse.clj
@@ -1,27 +1,21 @@
 (ns storehouse)
 
-(defn remove [result list element]
-  (if (empty? list)
-    result
-    list ))
-
-(defn new-shelf[capacity] {:products '() :capacity capacity})
+(defn new-shelf [capacity] {:products '() :capacity capacity})
 
 (defn put-in [shelf product]
-  (if (= (count (shelf :products)) (shelf :capacity))
+  (if (= (count (shelf :products )) (shelf :capacity ))
     (throw (Exception. "Shelf is full."))
-    (assoc shelf :products (conj (shelf :products) product))))
+    (assoc shelf :products (conj (shelf :products ) product))))
 
 (defn take-out [shelf product]
-    (assoc shelf :products (- (shelf :products) (list product))))
+  (assoc shelf :products (remove #(= % product) (shelf :products ))))
 
 (def s1 (new-shelf 2))
-(def s2 (put-in s1 :book))
-(def s3 (put-in s2 :printer))
+(def s2 (put-in s1 :book ))
+(def s3 (put-in s2 :printer ))
 
 (println s3)
 
-(def s4 (take-out s3 :book))
+(def s4 (take-out s3 :book ))
 
 (println s4)
-(println "DOES NOT WORK!")
