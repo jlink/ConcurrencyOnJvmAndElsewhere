d3a3e6cebb0bf568a315ab96452973a516b55307
Johannes Link <business@johanneslink.net>
2011-11-27 11:03:53 +0100

    Clojure storehouse can add and update shelves

--- src-clj/storehouse.clj
+++ src-clj/storehouse.clj
@@ -1,6 +1,4 @@
-(ns storehouse)
-
-(defn new-shelf [capacity] {:products '() :capacity capacity})
+(defn empty-shelf [capacity] {:products '() :capacity capacity})
 
 (defn put-in [shelf product]
   (if (= (count (shelf :products )) (shelf :capacity ))
@@ -10,12 +8,42 @@
 (defn take-out [shelf product]
   (assoc shelf :products (remove #(= % product) (shelf :products ))))
 
-(def s1 (new-shelf 2))
-(def s2 (put-in s1 :book ))
-(def s3 (put-in s2 :printer ))
+;(def s1 (empty-shelf 2))
+;(def s2 (put-in s1 :book ))
+;(def s3 (put-in s2 :printer ))
 
-(println s3)
+;(println s3)
 
-(def s4 (take-out s3 :book ))
+;(def s4 (take-out s3 :book ))
+;(println s4)
 
-(println s4)
+(defn empty-storehouse [] (ref {}))
+
+(defn add-shelf [map name shelf]
+  (assoc map name shelf))
+
+(defn replace-shelf [map name shelf]
+  (add-shelf map name shelf))
+
+(defn new-shelf [store name capacity]
+  (dosync
+    (alter store add-shelf name (empty-shelf capacity))))
+
+(defn update-shelf [store shelf-map]
+  (dosync
+    (let [shelf-name (first (keys shelf-map))
+          shelf (shelf-map shelf-name)]
+      (alter store replace-shelf shelf-name shelf))))
+
+(def store (empty-storehouse))
+
+(new-shelf store :a 2)
+(new-shelf store :b 3)
+
+(println @store)
+
+(def shelf-a (put-in (@store :a) "a book"))
+(update-shelf store {:a shelf-a} )
+
+(println @store)
+
