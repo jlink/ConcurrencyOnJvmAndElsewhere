3b8bb47faf96494c0b7676a58fe20149065a147f
Johannes Link <business@johanneslink.net>
2011-11-28 15:03:33 +0100

    Working on erlang storhouse

--- src-erl/storehouse.erl
+++ src-erl/storehouse.erl
@@ -1,5 +1,20 @@
 - module(storehouse) .
-- export([ fac / 1 ]) .
+- export([ new_shelf / 0 , put_in / 2 ]) .
+- export([ start / 0 , stop / 0 , init / 0 ]) .
 
-fac (1) -> 1 ;
-fac (N) -> N * fac (N - 1) .
+new_shelf () ->[ ] .
+
+put_in (Shelf , Product) ->
+[ Product | Shelf ] .
+
+start() ->
+    .
+
+init() ->
+    .
+
+stop() ->
+    .
+
+loop(Shelves) ->
+    receive
