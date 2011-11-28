74cd7e8fdc48ddd2e07197086d1f25b1cdfccbf7
Johannes Link <business@johanneslink.net>
2011-11-28 15:27:29 +0100

    Working on erlang storehouse...

--- src-erl/storehouse.erl
+++ src-erl/storehouse.erl
@@ -1,20 +1,37 @@
 - module(storehouse) .
 - export([ new_shelf / 0 , put_in / 2 ]) .
 - export([ start / 0 , stop / 0 , init / 0 ]) .
+- export([ call / 1 ]) .
 
 new_shelf () ->[ ] .
 
 put_in (Shelf , Product) ->
-[ Product | Shelf ] .
+    [ Product | Shelf ] .
 
-start() ->
-    .
+start () ->
+    register(storehouse, spawn(storehouse, init, [])).
 
 init() ->
-    .
+    loop([]).
 
 stop() ->
-    .
+    call(stop).
 
 loop(Shelves) ->
     receive
+        {request, From, Msg} when(Msg /= stop) ->
+            io:format("received: ~w~n", [Msg]),
+            reply(From, ok),
+            loop(Shelves);
+        {request, From, stop} ->
+            reply(From, ok)
+    end.
+
+call(Message) ->
+    storehouse ! {request, self(), Message},
+    receive
+        {reply, Reply} -> Reply
+    end.
+
+reply(Pid, Reply) ->
+    Pid ! {reply, Reply}.
